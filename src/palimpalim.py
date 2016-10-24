fe575fe4eafd99fe577204651dbf5a2609442136
chrys87 <chrys87@web.de>
2016-10-24 17:39:22 +0200

    Update palimpalim.py

--- src/palimpalim.py
+++ src/palimpalim.py
@@ -1,8 +1,12 @@
 #!/bin/python
+# -*- coding: utf-8 -*-
+
+# simple daemon that digitalize speaker bells/ beeps (char 7)
+# By Chrys, Storm Dragon, and contributers.
 import evdev
 from evdev import InputDevice
 from select import select
-import time
+#from . import driver
 
 devices = map(evdev.InputDevice, (evdev.list_devices()))
 devices = {dev.fd: dev for dev in devices}
@@ -10,11 +14,12 @@ devices = {dev.fd: dev for dev in devices}
 for fd in devices:
     for i in devices[fd].capabilities(True):
         print(devices[fd].fn,devices[fd].name,i)
+
 while True:
     r, w, x = select(devices, [], [])
     if r != []:
         for fd in r:
             for event in devices[fd].read():
-                print('Devicename:'+ devices[fd].name + '  Devicepath:' + devices[fd].fn + '  Events:' + str(devices[fd].active_keys(verbose=True)) + '  Value:' + str(event.value))
+                print('Devicename:'+ devices[fd].name + '  Devicepath:' + devices[fd].fn + ' | EVENTINFO Type: ' + str(event.type) + '  Code: ' + str(event.code) + '  Value: ' + str(event.value))
 
 
