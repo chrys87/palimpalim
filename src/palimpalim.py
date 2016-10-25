9fca91bce8defc0d30d8ba0caa9152b38533ad6c
chrys87 <chrys87@web.de>
2016-10-25 17:37:46 +0200

    add LEDs, clean up, add debugging

--- src/palimpalim.py
+++ src/palimpalim.py
@@ -6,6 +6,7 @@
 import evdev
 from evdev import InputDevice
 from select import select
+import time
 from configparser import ConfigParser
 #from . import driver
 
@@ -13,9 +14,14 @@ class palimpalim():
     __init__():
         self._config = None
         self.devices = None
+        self.debug = True
         self.announceLed = False
         self.announeBell = True
         self.loadSettings()
+        if self.announceLed:
+            self.currNumlock = self.getLedState()
+            self.currShift = self.getLedState(1) 
+            self.currScrolllock = self.getLedState(2)
     def loadSettings(self, settingConfigPath ='/etc/palim/settings.conf'):
         if not os.path.exists(settingConfigPath):
             return
@@ -45,13 +51,46 @@ class palimpalim():
         for fd in self.devices:
             for i in self.devices[fd].capabilities(True):
                 print(self.devices[fd].fn,self.devices[fd].name,i)
-    
+
+    def getLedState(self, led = 0):
+        # 0 = Numlock
+        # 1 = Capslock
+        # 2 = Rollen
+        if self.devices == None:
+            return False
+        if self.devices == {}:
+            return False                   
+        for fd, dev in self.devices.items():
+            return led in dev.leds()
+        return False 
+                
     def proceed(self):
         while True:
             r, w, x = select(self.devices, [], [])
+            # wait until all events reach it destination and did what they shouldwitch LEDs and so on)
+            time.sleep(0.15)
             if r != []:
                 for fd in r:
                     for event in self.devices[fd].read():
-                        print('Devicename:'+ self.devices[fd].name + '  Devicepath:' + self.devices[fd].fn + ' | EVENTINFO Type: ' + str(event.type) + '  Code: ' + str(event.code) + '  Value: ' + str(event.value))
+                        if event.type == 11: #EV_LED
+                            if self.announceLed:
+                                if self.currNumlock != self.getLedState():
+                                    if self.debug:
+                                        print('numlock",self.getLedState())
+                                    self.currNumlock = self.getLedState()
+                                if self.currShift != self.getLedState(1):
+                                    if self.debug:
+                                        print('shift",self.getLedState(1))
+                                    self.currShift = self.getLedState(1) 
+                                if self.currScrolllock != self.getLedState(2):
+                                    if self.debug:
+                                        print('scrolllock",self.getLedState(2))
+                                    self.currScrolllock = self.getLedState(2)
+                        if self.announeBell:
+                            if event.type == 12: #EV_SND
+                                if self.debug:
+                                    print('bell')
+                        if self.debug:
+                            print('Devicename:'+ self.devices[fd].name + '  Devicepath:' + self.devices[fd].fn + ' | EVENTINFO Type: ' + str(event.type) + '  Code: ' + str(event.code) + '  Value: ' + str(event.value))
 
 
